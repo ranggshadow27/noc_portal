dc8ebf177eaf6f48375ae8ef1a705742868e0312
rangga <46300525+ranggshadow27@users.noreply.github.com>
2024-12-19 05:46:54 +0700

    Add files via upload

---
+++ nocport/autochat/js/chat_template.js
@@ -0,0 +1,19 @@
+// Generate the chat format
+const initialChat =
+  `Selamat ${greeting} ${picGender}\n\n` +
+  `Kami dari tim NOC Mahaga, penyedia perangkat WiFi Bakti di lokasi ${namaSiteSopan}. 📡\n\n ` +
+  `Berdasarkan pemantauan kami, layanan WiFi Bakti di lokasi ${picGender} saat ini mengalami gangguan dan tidak dapat digunakan, ` +
+  `Apakah sedang ada masalah atau kendala pada perangkat kami ${picGender}?\n\n` +
+  `Mohon dibantu konfirmasinya ya ${picGender}, Kami siap membantu agar layanan WiFi dapat kembali digunakan dengan baik.\n\n` +
+  `Terima kasih banyak sebelumnya yaa ${picGender}. 🙏`;
+
+const pushChat =
+  `Selamat ${greeting} ${picGender}\n\n` +
+  `Mohon maaf mengganggu waktunya ${picGender} 🙏\n\n` +
+  `Kami dari tim NOC Mahaga ingin menyampaikan terkait layanan WiFi Bakti di lokasi ${picGender} yang saat ini masih terpantau mengalami gangguan. ` +
+  `Mohon kiranya ${picGender} dapat membantu kami dengan pengecekan awal pada perangkat di lokasi. 🙏 \n\n` +
+  `Apabila pengecekan tersebut tidak memungkinkan, ` +
+  `mohon bantuannya ya ${picGender} untuk menginformasikan kontak alternatif yang dapat kami hubungi agar koordinasi pengecekan perangkat dapat segera dilakukan. \n\n` +
+  `Jika diperlukan penanganan teknis lebih lanjut, kami siap mengirimkan teknisi ke lokasi ${picGender} dan melakukan penggantian perangkat jika dibutuhkan. ` +
+  `Namun, untuk langkah awal, kami sangat menghargai bantuan ${picGender} dalam pengecekan ini. \n\n` +
+  `Terima kasih banyak atas bantuan dan kerja samanya ${picGender}. 🙏`;
